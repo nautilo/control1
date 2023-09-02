5e422a5f171414a2096cab938d2a51196ac5890a
Fabisb13 <128733117+Fabisb13@users.noreply.github.com>
2023-09-01 20:37:37 -0400

    Páginas html construidas

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -1,2 +1,90 @@
 export class Usuario {
-}
+    public correo: string;
+    public password: string;
+    public preguntaSecreta: string;
+    public respuestaSecreta: string;
+    public nombreCompleto: string; // Nuevo atributo
+  
+    constructor(
+      correo: string,
+      password: string,
+      preguntaSecreta: string,
+      respuestaSecreta: string,
+      nombreCompleto: string, // Nuevo atributo
+    ) {
+      this.correo = correo;
+      this.password = password;
+      this.preguntaSecreta = preguntaSecreta;
+      this.respuestaSecreta = respuestaSecreta;
+      this.nombreCompleto = nombreCompleto; // Nuevo atributo
+    }
+  
+    public getCorreo(): string {
+      return this.correo;
+    }
+  
+    public getPassword(): string {
+      return this.password;
+    }
+  
+    public setUsuario(correo: string, password: string): void {
+      this.correo = correo;
+      this.password = password;
+    }
+  
+    public listaUsuariosValidos(): Usuario[] {
+      const lista = [];
+      lista.push(
+        new Usuario(
+          'sin.datos@duocuc.cl',
+          '1234',
+          '',
+          '',
+          'Nombre Sin Datos' // Ejemplo de nombre completo
+        )
+      );
+      // Resto de usuarios...
+      return lista;
+    }
+  
+    public buscarUsuarioValido(correo: string, password: string): Usuario | undefined {
+      const nived: Usuario | undefined = this.listaUsuariosValidos().find(
+        (usu) => usu.correo === correo && usu.password === password
+      );
+      return nived;
+    }
+  
+    public validarCorreo(): string {
+      const patronCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (patronCorreo.test(this.correo)) {
+        return '';
+      } else {
+        return 'El correo ingresado no tiene un formato válido.';
+      }
+    }
+  
+    public validarPassword(): string {
+      if (this.password.trim() === '') {
+        return 'Para entrar al sistema debe ingresar una contraseña.';
+      }
+      for (let i = 0; i < this.password.length; i++) {
+        if ('0123456789'.indexOf(this.password.charAt(i)) === -1) {
+          return 'La contraseña debe ser numérica.';
+        }
+      }
+      if (this.password.length !== 4) {
+        return 'La contraseña debe ser numérica de 4 dígitos.';
+      }
+      return '';
+    }
+  
+    public validarCredenciales(): string {
+      const usu: Usuario | undefined = this.buscarUsuarioValido(this.correo, this.password);
+      return usu ? '' : 'El usuario no fue encontrado en el sistema.';
+    }
+  
+    public validarUsuario(): string {
+      return this.validarCorreo() || this.validarPassword() || this.validarCredenciales();
+    }
+  }
+  
--- src/app/pages/correcto/correcto.page.ts
+++ src/app/pages/correcto/correcto.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router'; // Permite navegar y pasar parámetros extra entre páginas
+import { ToastController } from '@ionic/angular'; // Permite mostrar mensajes emergente
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-correcto',
@@ -7,9 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CorrectoPage implements OnInit {
 
-  constructor() { }
+  // CGV: La clase typescript "IngresoPage" es la encargada de implementar las reglas de negocio de la página.
+  // Las propiedades del archivo typescript pueden intercambiar valores con los elementos HTML, por medio
+  // de "Modelo Angular". Por ejemplo, el siguiente TAG se enlaza con la propiedad "usuario.correo":
+  //   <ion-input type="text" [(ngModel)]="usuario.correo"></ion-input>
+  // Gracias a este modelo, cada vez que cambia la caja de texto entonces cambia la propiedad y viceversa.
 
-  ngOnInit() {
+  public usuario: Usuario;
+
+  // CGV: Para poder trabajar con Router y poder navegar hacia la página "home", debemos primero pasar como
+  // parámetro e instanciar un objeto de la clase "Router". Fijarse que el tipo de dato, que se pasa 
+  // en el constructor es "Router" con mayúscula, porque se trata de una clase y éstas parten con letra 
+  // mayúscula, mientras que "router" con minúscula es el objeto de esa clase, que usaremos para ejecutar
+  // el método "navigate". La creación de parámetros "private" en el constructor se llama 
+  // "Inyección de Dependencia" y es una práctica recomendada en Angular, que permite crear el objeto
+  // como una propiedad más de la página y así poder usarla. Por otro lado, la "Inyección de Dependencia"
+  // permite compartir una única instancia de dicho objeto en el resto de las páginas que lo usen. Lo
+  // anterior es especialmente importante para mantener la coherencia y estados compartidos en los Servicios.
+  
+  constructor(private router: Router, private toastController: ToastController) {
+    this.usuario = new Usuario('', '', '', '', '')
+  
+    // Puedes descomentar cualquiera de los siguientes usuarios, para 
+    // hacer tus pruebas y así no tener que digitarlos a cada rato
+
+    // this.usuario.setUsuario('sin.datos@duocuc.cl', '1234');
+    this.usuario.setUsuario('atorres@duocuc.cl', '1234');
+    // this.usuario.setUsuario('jperez@duocuc.cl', '5678');
+    // this.usuario.setUsuario('cmujica@duocuc.cl', '0987');
+    // this.usuario.setUsuario('usuario.inexistente@duocuc.cl', '1234');
+    // this.usuario.setUsuario('atorres@duocuc.cl', 'password mala');
+    // this.usuario.setUsuario('atorres@duocuc.cl', '9999999999999');
+    // this.usuario.setUsuario('atorres@duocuc.cl', '9999');
+    // this.usuario.setUsuario('correo.malo', '0987');
+    // this.usuario.setUsuario('correo.malo@', '0987');
+    // this.usuario.setUsuario('correo.malo@duocuc', '0987');
+    // this.usuario.setUsuario('correo.malo@duocuc.', '0987');
+  }
+
+  public ngOnInit(): void {
+
+    // Puedes descomentar la siguiente línea si quieres que la aplicación navegue directamente
+    // a la página Home, así te ahorras de estar apretando el botón "Ingresar" a cada rato
+    
+    //if (this.usuario.correo !== '') this.ingresar();
+  }
+
+  public ingresar(): void {
+    
+    if (this.usuario) {
+      
+      // Validamos el usuario y si hay error no navegaremos a la página Home
+      const mensajeError = this.usuario.validarUsuario();
+      if (mensajeError) {
+        this.mostrarMensaje(mensajeError);
+        return;
+      }
+
+      // Como la página sólo permite ingresar el correo y la password, vamos a buscar el usuario para completar sus datos
+      const usu: Usuario | undefined = this.usuario.buscarUsuarioValido(this.usuario.correo, this.usuario.password);
+      
+      if (usu) {
+        // NavigationExtras sirve para pasarle parámetros a la página Home. Los parámetros se agregan al objeto "state"
+        const navigationExtras: NavigationExtras = {
+          state: {
+            usuario: usu
+          }
+        };
+        this.mostrarMensaje(`¡Bienvenido(a) ${usu.nombreCompleto}!`);
+        this.router.navigate(['/home'], navigationExtras); // Navegamos hacia el Home y enviamos la información extra
+      }
+    }
+  }
+
+  async mostrarMensaje(mensaje: string, duracion?: number) {
+    // Permite mostrar un mensaje emergente que dura unos pocos segundos y desaparece. El mensaje es asincrónico, 
+    // los que permite que el mensaje se pueda ver incluso cuando ya ha cambiado a la siguiente página.
+    const toast = await this.toastController.create({
+        message: mensaje,
+        duration: duracion? duracion: 2000
+      });
+    toast.present();
   }
 
 }
